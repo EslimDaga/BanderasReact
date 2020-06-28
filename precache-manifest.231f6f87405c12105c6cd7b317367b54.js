408e572a03159aaec88ec47ab0ca24e2cf8c4434
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2020-06-28 18:01:07 -0500

    Updates

---
+++ precache-manifest.231f6f87405c12105c6cd7b317367b54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e850860f9003a16cf2320e7cea3285b7",
+    "url": "/BanderasReact/index.html"
+  },
+  {
+    "revision": "1c0c09595d3a2147cd42",
+    "url": "/BanderasReact/static/css/main.392e0663.chunk.css"
+  },
+  {
+    "revision": "2ba83b4b0db9eb4140ad",
+    "url": "/BanderasReact/static/js/2.6c7001ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BanderasReact/static/js/2.6c7001ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c0c09595d3a2147cd42",
+    "url": "/BanderasReact/static/js/main.bf171fbb.chunk.js"
+  },
+  {
+    "revision": "7251d3346d9ea25b8012",
+    "url": "/BanderasReact/static/js/runtime-main.32ce4e4e.js"
+  }
+]);
